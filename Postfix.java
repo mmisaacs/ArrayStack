890db78a9832d895a3376baabb40de99d33c55d2
Maddie Isaacs <misaacs@cpp.edu>
2023-11-06 21:29:13 -0800

    completed the postfix???

--- Postfix.java
+++ Postfix.java
@@ -1,31 +1,63 @@
+import java.nio.charset.CharacterCodingException;
+
 public class Postfix {
+    static int precedence(char c){
+        switch (c){
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
     public static String convertToPostfix(String infix){
         StackInterface<Character> operatorStack = new ResizeableArrayStack<>();
+        int i = 0;
         StringBuilder postfix = new StringBuilder();
 
-        int i = 0;
         while (i < infix.length()) {
-            char character = infix.charAt(i);
+            char c = infix.charAt(i);
             char nextCharacter = infix.charAt(i + 1);
-            if (Character.isWhitespace(nextCharacter)){
-                nextCharacter = infix.charAt(i + 2);
-            }
-            switch (character){
+
+            switch (c){
                 case '^' :
                     operatorStack.push(nextCharacter);
                     break;
 
-                case '+':
-                case '-':
-                case '*':
-                case '/':
+                case '+', '-', '*', '/':
                     while(!operatorStack.isEmpty() &&
-                            (nextCharacter <= operatorStack.peek())){
-                        postfix.append(operatorStack.peek());
+                            (precedence(nextCharacter) <= precedence(operatorStack.peek()))){
+                        postfix.append(operatorStack.pop());
                     }
+                    operatorStack.push(nextCharacter);
+                    break;
+
+                case '(':
+                    operatorStack.push(nextCharacter);
+                    break;
+
+                case ')':
+                    Character topOperator = operatorStack.pop();
+                    while(topOperator != '('){
+                        postfix.append(topOperator);
+                        topOperator = operatorStack.pop();
+                    }
+                    break;
+
+                default:
+                    postfix.append(nextCharacter);
+                    break;
             }
             i++;
         }
-        return "";
+        while (!operatorStack.isEmpty()){
+            Character topOperator = operatorStack.pop();
+            postfix.append(topOperator);
+        }
+        return postfix.toString();
     }
 }
