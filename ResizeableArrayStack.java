e0f825dfe3cb3dd7976778b8537ca523db17ab38
Maddie Isaacs <misaacs@cpp.edu>
2023-11-07 00:22:26 -0800

    Implement stack methods

--- ResizeableArrayStack.java
+++ ResizeableArrayStack.java
@@ -1,24 +1,61 @@
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
 public class ResizeableArrayStack<T> implements StackInterface<T> {
-    public void push(T newEntry) {
-        //FINISH
+    private T[] stack;
+    private int topIndex;
+    private static int defaultCap = 25;
+
+    public ResizeableArrayStack(){
+        this(defaultCap);
     }
+
+    public ResizeableArrayStack(int initialCapacity){
+        @SuppressWarnings("unchecked")
+        T[] tempStack = (T[]) new Object[initialCapacity];
+        stack = tempStack;
+        topIndex = -1;
+    }
+
+    private void checkCapacity(){
+        if(topIndex >= stack.length -1){
+            int newLength = 2 * stack.length;
+            stack = Arrays.copyOf(stack, newLength);
+        }
+    }
+    public void push(T newEntry) {
+        checkCapacity();
+        stack[topIndex + 1] = newEntry;
+        topIndex++;
+    } //end push
 
     public T pop() {
-        //FINISH
-        return null;
-    }
+        if(isEmpty())
+            throw new EmptyStackException();
+        else {
+            T top = stack[topIndex];
+            stack[topIndex] = null;
+            topIndex--;
+            return top;
+        } //end else
+    } //end pop
 
     public T peek() {
-        //FINISH
-        return null;
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        else
+            return stack[topIndex];
     }
 
     public boolean isEmpty() {
-        //FINISH
-        return false;
+        return topIndex < 0;
     }
 
     public void clear() {
-        //FINISH
-    }
+        while(topIndex > -1){
+            stack[topIndex] = null;
+            topIndex--;
+        } //end while
+    } //end clear
 }
